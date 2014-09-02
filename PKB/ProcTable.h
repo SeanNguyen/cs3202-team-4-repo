1575b6ba6c132eb2c994c4559a76c54ba8bd27c4
unknown <missa4892@gmail.com>
2014-09-02 22:41:30 +0800

    [PKB] added procTable and corrected initialization of 2d vectors

---
+++ PKB/ProcTable.h
@@ -0,0 +1,26 @@
+#ifndef PROCTABLE_H_INCLUDED
+#define PROCTABLE_H_INCLUDED
+
+#include <vector>
+#include <string>
+using namespace std;
+
+class ProcTable
+{
+private: 
+	vector<string> procTable;
+	
+
+public:
+	
+	int insertProc(string name);
+
+	vector<int> getProcIndex(string name);
+
+	string getProcName(int index);
+
+	int getSize();
+	
+};
+
+#endif
---
+++ PKB/TestFollow.h
@@ -0,0 +1,52 @@
+//#ifndef TestFollow_h
+//#define TestFollow_h
+//
+//// Note 1
+//#include <cppunit/extensions/HelperMacros.h>
+//
+//class FollowTest : public CPPUNIT_NS::TestFixture // Note 2 
+//{ 
+//	CPPUNIT_TEST_SUITE( FollowTest ); // Note 3 
+//	CPPUNIT_TEST( testInsert );
+//	CPPUNIT_TEST( testisFollow );
+//	CPPUNIT_TEST( testisFollowStar );
+//
+//	CPPUNIT_TEST( testgetFollowing ) ;
+//	CPPUNIT_TEST( testgetFollowed ) ;
+//	//CPPUNIT_TEST( testgetSize ) ;
+//	CPPUNIT_TEST( testgetFollowingStarStmt );
+//	CPPUNIT_TEST( testgetFollowedStarStmt );
+//	CPPUNIT_TEST( testgetAllFollowingStmt );
+//	CPPUNIT_TEST( testgetAllFollowedStmt );
+//	CPPUNIT_TEST_SUITE_END();
+//
+//public:
+//	void setUp();
+//	void tearDown();
+//
+//	// method to test insert
+//	void testInsert();
+//
+//	//method to test follow
+//	void testisFollow();
+//
+//	void testisFollowStar();
+//
+//	////method to test follow
+//	void testgetFollowing();
+//
+//	void testgetFollowed();
+//
+//	//void testgetSize();
+//	//	
+//	void testgetFollowingStarStmt();
+//
+//	void testgetFollowedStarStmt();
+//
+//	void testgetAllFollowingStmt();
+//
+//	void testgetAllFollowedStmt();
+//
+//};
+//#endif
+//    
