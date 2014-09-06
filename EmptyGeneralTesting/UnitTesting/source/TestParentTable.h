763b227c2f33ff0c4fe499cf5288f48ab55481b1
unknown <missa4892@gmail.com>
2014-09-06 14:47:43 +0800

    [PKB]corrected links to unitTesting

---
+++ EmptyGeneralTesting/UnitTesting/source/TestParentTable.h
@@ -0,0 +1,38 @@
+//#ifndef TestParentTable_h
+//#define TestParentTable_h
+//
+//#include <cppunit/extensions/HelperMacros.h>
+//
+//class ParentTableTest : public CPPUNIT_NS::TestFixture // Note 2 
+//{ 
+//	CPPUNIT_TEST_SUITE( ParentTableTest ); // Note 3 
+//	CPPUNIT_TEST(testInsertParent);
+//	CPPUNIT_TEST(testIsParent);
+//	CPPUNIT_TEST(testIsParentStar);
+//	CPPUNIT_TEST(testGetParentStmt);
+//	CPPUNIT_TEST(testGetChildStmt);
+//	CPPUNIT_TEST(testGetChildStarStmt);
+//	CPPUNIT_TEST(testGetParentStarStmt);
+//	CPPUNIT_TEST(testGetAllParentStmt);
+//	CPPUNIT_TEST(testGetAllChildStmt);
+//
+//	CPPUNIT_TEST_SUITE_END();
+//
+//public:
+//	void setUp();
+//	void tearDown();
+//	void testInsertParent();
+//	void testIsParent();
+//	void testIsParentStar();
+//	void testGetParentStmt();
+//	void testGetChildStmt();
+//	void testGetChildStarStmt();
+//	void testGetParentStarStmt();
+//	void testGetAllParentStmt();
+//	void testGetAllChildStmt();
+//
+//	
+//	
+//};
+//
+//#endif
