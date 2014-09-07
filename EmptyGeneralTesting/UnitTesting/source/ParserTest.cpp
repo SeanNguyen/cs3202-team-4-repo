fc05767a0f0a2bec80e2bdaca5991155fe0065da
SeanNguyen <firestormkjc.1993@gmail.com>
2014-09-07 23:47:21 +0800

    Added more Parser Test Cases

--- EmptyGeneralTesting/UnitTesting/source/ParserTest.cpp
+++ EmptyGeneralTesting/UnitTesting/source/ParserTest.cpp
@@ -9,7 +9,13 @@
 
 using namespace std;
 
-string testFile = "..\\UnitTesting\\test cases\\Source1.txt";
+string testFile_EmptyProcedure = "..\\UnitTesting\\test cases\\ParserTest_EmptyProceure.txt";
+string testFile_Call = "..\\UnitTesting\\test cases\\ParserTest_Call.txt";
+string testFile_While = "..\\UnitTesting\\test cases\\ParserTest_While.txt";
+string testFile_If = "..\\UnitTesting\\test cases\\ParserTest_If.txt";
+string testFile_SimpleAssign = "..\\UnitTesting\\test cases\\ParserTest_SimpleAssign.txt";
+string testFile_ComplexAssign = "..\\UnitTesting\\test cases\\ParserTest_ComplexAssign.txt";
+
 void ParserTest::setUp() {
 }
 
@@ -18,7 +24,32 @@ void ParserTest::tearDown() {
 
 CPPUNIT_TEST_SUITE_REGISTRATION( ParserTest );
 
-void ParserTest::testParse() {
+void ParserTest::testParseEmptyProcedure() {
 	Parser parser;
-	parser.parse(testFile);
+	parser.parse(testFile_EmptyProcedure);
+}
+
+void ParserTest::testParseCall() {
+	Parser parser;
+	parser.parse(testFile_Call);
+}
+
+void ParserTest::testParseWhile() {
+	Parser parser;
+	parser.parse(testFile_While);
+}
+
+void ParserTest::testParseIf() {
+	Parser parser;
+	parser.parse(testFile_If);
+}
+
+void ParserTest::testParseSimpleAssign() {
+	Parser parser;
+	parser.parse(testFile_SimpleAssign);
+}
+
+void ParserTest::testParseComplexAssign() {
+	Parser parser;
+	parser.parse(testFile_ComplexAssign);
 }
