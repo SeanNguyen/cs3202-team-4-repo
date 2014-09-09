57a9f7f4812d5a5424d15a453e79a96528ceb452
VuPhucTho <vuphuctho93@gmail.com>
2014-09-09 15:21:56 +0800

    modify Tree based on new TNode

--- EmptyGeneralTesting/source/Tree.h
+++ EmptyGeneralTesting/source/Tree.h
@@ -5,14 +5,14 @@
 class Tree {
 public:
 	Tree();
-	TNode & createNode();
-	TNode & createNode(Symbol type);
-	TNode & createNode(Symbol type, string value);
-	int addChild(TNode parent, TNode child);
-	void setRoot(TNode node);
-	TNode & getRoot();
-	int getNumChildren(TNode node);
-	TNode & getChildAtIndex(TNode node, int i);
+	TNode *createNode();
+	TNode *createNode(Symbol type);
+	TNode *createNode(Symbol type, string value);
+	int addChild(TNode &parent, TNode &child);
+	void setRoot(TNode &node);
+	TNode *getRoot();
+	int getNumChildren(TNode &node);
+	TNode *getChildAtIndex(TNode &node, int i);
 private:
-	TNode root;
+	TNode *root;
 };
