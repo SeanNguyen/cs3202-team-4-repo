a8f9c0390fd41c2a640d978d126ae90ecb06b6cb
VuPhucTho <vuphuctho93@gmail.com>
2014-09-09 16:27:58 +0800

    Finish TestTree

--- EmptyGeneralTesting/UnitTesting/TestTree.cpp
+++ EmptyGeneralTesting/UnitTesting/TestTree.cpp
@@ -23,13 +23,62 @@ void TreeTest::testCreateNode() {
 }
 
 void TreeTest::testRoot() {
+	Tree tree;
+	TNode root = tree.createNode();
+	tree.setRoot(root);
+
+	TNode _root = *tree.getRoot();
+
+	CPPUNIT_ASSERT(_root.getType()==Undefined);
+	CPPUNIT_ASSERT(_root.getValue()=="");
 }
 
 void TreeTest::testGetNumChildren() {
+	Tree tree;
+	// randomize some nodes
+	TNode root = tree.createNode(Program);
+	TNode node1 = tree.createNode(Procedure, "First");
+	TNode node1_1 = tree.createNode(StmtList);
+	TNode node1_2 = tree.createNode(While);
+	TNode node2 = tree.createNode(If);
+
+	tree.setRoot(root);
+	tree.addChild(root, node1);
+	tree.addChild(node1, node1_1);
+	tree.addChild(node1, node1_2);
+	tree.addChild(root, node2);
+
+	// testing
+	TNode _root = *tree.getRoot();
+	TNode _child1 = *tree.getChildAtIndex(_root, 0);
+
+	CPPUNIT_ASSERT_EQUAL(2, _root.getNumChildren());
+	CPPUNIT_ASSERT_EQUAL(2, _child1.getNumChildren());
 }
 
 void TreeTest::testGetChildAtIndex() {
 }
 
 void TreeTest::testAddChild() {
+	Tree tree;
+	// randomize some nodes
+	TNode root = tree.createNode(Program);
+	TNode node1 = tree.createNode(Procedure, "First");
+	TNode node1_1 = tree.createNode(StmtList);
+	TNode node1_2 = tree.createNode(While);
+	TNode node2 = tree.createNode(If);
+
+	tree.setRoot(root);
+	tree.addChild(root, node1);
+	tree.addChild(node1, node1_1);
+	tree.addChild(node1, node1_2);
+	tree.addChild(root, node2);
+
+	// testing
+	TNode _root = *tree.getRoot();
+	TNode _child1 = *tree.getChildAtIndex(_root, 0);
+
+	CPPUNIT_ASSERT_EQUAL(2, _root.getNumChildren());
+	CPPUNIT_ASSERT_EQUAL(Procedure, _child1.getType());
+	CPPUNIT_ASSERT_EQUAL(2, _child1.getNumChildren());
 }
