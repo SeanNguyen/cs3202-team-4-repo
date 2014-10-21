b032d1943c8ae7d56106c49403a78a4eb2a4aae6
VuPhucTho <vuphuctho93@gmail.com>
2014-10-21 11:51:48 +0800

    fix sort symboltable - QO

--- Static Program Analyzer/UnitTesting/source/TestQueryPreprocessor.cpp
+++ Static Program Analyzer/UnitTesting/source/TestQueryPreprocessor.cpp
@@ -92,6 +92,6 @@ void QueryPreprocessorTest::testBuildComplexQuery1() {
 
 	Tree tree = QueryRepresentator::getQueryTree(0);
 	TNode * root = tree.getRoot();
-	/*cout << endl << "TEST BUILD TREE: Query: assign a1, a2; while w1, w2; Select a2 pattern a1(\"x\",_) and a2(\"x\",_\"x\"_) such that Affects(a1, a2) and Parent*(w2, a2) and Parent*(w1, w2)" <<endl;
-	root -> printTNode();*/
+	cout << endl << "TEST BUILD TREE: Query: assign a1, a2; while w1, w2; Select a2 pattern a1(\"x\",_) and a2(\"x\",_\"x\"_) such that Affects(a1, a2) and Parent*(w2, a2) and Parent*(w1, w2)" <<endl;
+	root -> printTNode();
 } 
--- Static Program Analyzer/UnitTesting/source/TestQueryPreprocessor.h
+++ Static Program Analyzer/UnitTesting/source/TestQueryPreprocessor.h
@@ -9,11 +9,11 @@ class QueryPreprocessorTest : public CPPUNIT_NS::TestFixture {
 	CPPUNIT_TEST( testReadFileData );
 	//CPPUNIT_TEST( testBuildSymbolTable );
 	//CPPUNIT_TEST( testBuildTree );
-	CPPUNIT_TEST( testBuildSuchThatCls );
+	//CPPUNIT_TEST( testBuildSuchThatCls );
 	//CPPUNIT_TEST( testBuildPatternCls );
 	//CPPUNIT_TEST( testBuildWithCls );
 	//CPPUNIT_TEST( testBuildTupleResult);
-	//CPPUNIT_TEST( testBuildComplexQuery1);
+	CPPUNIT_TEST( testBuildComplexQuery1);
 
 	CPPUNIT_TEST_SUITE_END();
 public:
