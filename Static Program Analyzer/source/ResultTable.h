79fb74ce44c230a3b9f158bd43c5cd61c01a520f
VuPhucTho <vuphuctho93@gmail.com>
2014-10-28 08:34:30 +0800

    add ResultManager

--- Static Program Analyzer/source/ResultTable.h
+++ Static Program Analyzer/source/ResultTable.h
@@ -1,19 +1,38 @@
 #pragma once
 
 #include <vector>
+#include <algorithm>
 #include "SyntaxHelper.h"
 
 class ResultTable {
 public:
 	ResultTable();
 	~ResultTable();
+	int getSymbolSize();
 	int getSymbolIndex(string name);
+	vector<string> getAllSymbols();
 	string getSymbol(int index);
 	bool containsSymbol(string name);
+	void insertSymbol(vector<string> s);
+	void insertSymbol(string s);
 
-	vector<int> getValueRow(int index);
+	bool containsValRow(vector<string> row);
+	int getValRowIndex(vector<string> row);
+	vector<string> getValRow(int index);
+	void insertValRow(vector<string> r);
+	void deleleInvalidRows();
+
+	ResultTable * extractData(vector<string> s);
 private:
 	// sth to store the names of symbols
+	vector<string> symbols;
 	// sth to store rows of values of above symbols
+	vector<vector<string>> valRows;
 	// sth more to rmb frquently used data (size, symbol#)
+	int size;
+	int symbolSize;
+
+
+	bool compareVector(vector<string> a, vector<string> b);
+	bool containsVector(vector<string> a, vector<string> b);
 };
