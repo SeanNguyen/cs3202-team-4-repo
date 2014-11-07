1003216496beee3c4aac9e861ffdb7dc1204c987
Azima M I <miazima@gmail.com>
2014-11-07 23:34:27 +0800

    integration testing

--- Static Program Analyzer/UnitTesting/TestNext.cpp
+++ Static Program Analyzer/UnitTesting/TestNext.cpp
@@ -46,7 +46,16 @@ void TestNext::TestisNext() {
 }
 
 void TestNext::TestIsNextStar() {
-	/*CPPUNIT_ASSERT( pkb.isNextStar(1, 4) );
+	string source = "..\\..\\Tests\\TestNextSource.txt";
+	Parser parser;
+	PKB pkb;
+	DesignExtractor designextractor;
+	
+	parser.parse(source);
+	parser.buildPKB();
+//	designextractor.buildPKB();
+
+	CPPUNIT_ASSERT( pkb.isNextStar(1, 4) );
 	CPPUNIT_ASSERT( pkb.isNextStar(1, 5) );
 	CPPUNIT_ASSERT( pkb.isNextStar(1, 11) );
 	CPPUNIT_ASSERT( pkb.isNextStar(1, 12) );
@@ -65,21 +74,20 @@ void TestNext::TestIsNextStar() {
 	CPPUNIT_ASSERT( pkb.isNextStar(2, 3) );
 	CPPUNIT_ASSERT( pkb.isNextStar(2, 6) );						
 	CPPUNIT_ASSERT( pkb.isNextStar(2, 9) );						
-	CPPUNIT_ASSERT( pkb.isNextStar(2, 3) );
 	
-	CPPUNIT_ASSERT( pkb.isNextStar(3, 6) );
+	CPPUNIT_ASSERT( pkb.isNextStar(3, 4) );
 	CPPUNIT_ASSERT( pkb.isNextStar(3, 7) );
 	CPPUNIT_ASSERT( pkb.isNextStar(3, 11) );
 	CPPUNIT_ASSERT( pkb.isNextStar(3, 12) );
 	CPPUNIT_ASSERT( pkb.isNextStar(3, 10) );
 	CPPUNIT_ASSERT( pkb.isNextStar(3, 9) );	
 
-	CPPUNIT_ASSERT( pkb.isNextStar(4, 5) );
-	CPPUNIT_ASSERT( pkb.isNextStar(4, 11) );
-	CPPUNIT_ASSERT( pkb.isNextStar(4, 12) );
-	CPPUNIT_ASSERT( pkb.isNextStar(4, 7) );
-	CPPUNIT_ASSERT( pkb.isNextStar(4, 10) );
-	CPPUNIT_ASSERT( pkb.isNextStar(4, 9) );						
+	CPPUNIT_ASSERT( pkb.isNextStar(5, 6) );
+	CPPUNIT_ASSERT( pkb.isNextStar(5, 11) );
+	CPPUNIT_ASSERT( pkb.isNextStar(5, 12) );
+	CPPUNIT_ASSERT( pkb.isNextStar(5, 7) );
+	CPPUNIT_ASSERT( pkb.isNextStar(5, 10) );
+	CPPUNIT_ASSERT( pkb.isNextStar(5, 9) );						
 
 	CPPUNIT_ASSERT( pkb.isNextStar(6, 11) );
 	CPPUNIT_ASSERT( pkb.isNextStar(6, 12) );
@@ -98,10 +106,29 @@ void TestNext::TestIsNextStar() {
 	CPPUNIT_ASSERT( pkb.isNextStar(9, 7) );
 	CPPUNIT_ASSERT( pkb.isNextStar(9, 11) );
 	CPPUNIT_ASSERT( pkb.isNextStar(9, 9) );
-	CPPUNIT_ASSERT( pkb.isNextStar(9, 12) );*/
+	CPPUNIT_ASSERT( pkb.isNextStar(9, 12) );
 }
 
 void TestNext::TestgetNextStmts() {
+	string source = "..\\..\\Tests\\TestNextSource.txt";
+	Parser parser;
+	PKB pkb;
+	DesignExtractor designextractor;
+	
+	parser.parse(source);
+	parser.buildPKB();
+//	designextractor.buildPKB();
+
+	int stmts[] = {2};
+	vector<int> nextstmts(stmts, stmts + sizeof(stmts) / sizeof(int) );
+	//sort(expectedvars.begin(), nextstmts.end()); 
+	CPPUNIT_ASSERT_EQUAL(nextstmts.at(0), (pkb.getNextStmts(1)).at(0));
+	//CPPUNIT_ASSERT_EQUAL(nextstmts.at(1), (pkb.getNextStmts(1)).at(1));
+
+	int stmts2[] = {3, 5};
+	vector<int> nextstmts2(stmts2, stmts2 + sizeof(stmts2) / sizeof(int) );
+	//sort(expectedvars.begin(), nextstmts.end()); 
+	CPPUNIT_ASSERT_EQUAL(nextstmts2.at(0), (pkb.getNextStmts(2)).at(0));
 
 }
 
